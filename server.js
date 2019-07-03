d6a53bb54954ebba307a7dd3a68f0f9050a0167e
Michael Yohannes <yohannesbergnov@gmail.com>
2019-07-03 09:01:59 -0400

    server port changed to allow for environment variable

--- server.js
+++ server.js
@@ -25,9 +25,7 @@ var salt = bcrypt.genSaltSync(saltRounds);
 app.use(bodyParser.json());
 app.use(cors());
  
-app.get("/", (req,res) => {
-    res.json(database.users);
-});
+app.get("/", (req,res) => {res.json("index root"); });
 
 app.post("/signin", signin.handleSignInRoute(bcrypt, db));
 
@@ -39,6 +37,6 @@ app.put("/image", image.handleImageRoute(db));
 
 app.post("/imageface", image.handleApiRoute);
 
-app.listen(3000, () => {
+app.listen( process.env.PORT || 3000, () => {
     console.log("SERVER LIVING");
 });
